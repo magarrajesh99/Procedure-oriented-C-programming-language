70c792555302447e4981eeb132a9231049a03b93
magarrajesh99 <87904261+magarrajesh99@users.noreply.github.com>
2021-09-08 22:48:47 +0530

    Basic while loop

---
+++ program17.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+ 
+void DisplayFactors(int);
+
+int main()
+{
+	int iNo=0;
+	
+	printf("Enter number\n");
+	scanf("%d",&iNo);
+	
+	DisplayFactors(iNo);
+	
+	return 0;
+}
+
+void DisplayFactors(int iValue)
+{
+	
+	int iCnt=0;
+	if(iValue<0)
+	{
+		iValue=-iValue;
+	}
+	for(iCnt=1;iCnt<(iValue/2);iCnt++)		//O(N/2)
+	{
+		if((iValue%iCnt)==0)
+		{
+			printf("%d\t",iCnt);
+		}
+	}
+}
