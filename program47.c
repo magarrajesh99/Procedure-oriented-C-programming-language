9e50844a0131f65025c22dfc82557bf96e7fd1c4
magarrajesh99 <87904261+magarrajesh99@users.noreply.github.com>
2021-09-21 22:27:08 +0530

    Pattern displaying

---
+++ program47.c
@@ -0,0 +1,30 @@
+
+//Input:	5
+//Output:	a 	b  c  d  e
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+	int iCnt=0;
+	char ch='\0';
+	
+	for(iCnt=1,ch='a';iCnt<=iNo;iCnt++,ch++)
+	{
+		printf("%c\t",ch);
+		
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int iValue=0;
+	
+	printf("Enter number of elements\n");
+	scanf("%d",&iValue);
+	
+	Pattern(iValue);
+	
+	return 0;
+}
